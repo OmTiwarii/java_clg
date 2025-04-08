12d42c0b3a2df18094f69e90c247a1c8f1f6f921
Dell <Dell@MacBook-Air.local>
2025-04-08 11:13:48 +0530

    hel

--- javah.java
+++ javah.java
@@ -1,1 +1,17 @@
-class helloworld 
+import java.util.Scanner;
+class firstclass{
+    public static void main (String args[] ){
+        //input
+        System.out.println("hello world daddy comes");
+     /*   System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");*/
+        //output
+       // Scanner sc = new Scanner(System.in);
+       // String name = sc.next();//single word 
+       // String fname = sc.nextLine(); //full sentence //type nextint(),nextfloat();
+       // System.out.println(name);
+    //  System.out.println(fname);
+    }
+}
