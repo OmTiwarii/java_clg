e1fb1416c03d4bce20b212c15aae1934fbb72b5a
Dell <Dell@MacBook-Air.local>
2025-04-11 13:15:16 +0530

    om

--- firstclass.java
+++ firstclass.java
@@ -1,5 +1,6 @@
 //import java.util.Scanner;
 
+import java.nio.channels.Pipe.SourceChannel;
 import java.util.Scanner;
 
 class firstclass{
@@ -47,7 +48,7 @@ if(a>b && a>c){
     System.out.println("C is greater");
 }
 */
-Scanner year = new Scanner(System.in);
+/*Scanner year = new Scanner(System.in);
 double  i = year.nextDouble();
 if(i==366){
     System.out.println("leap year");
@@ -56,5 +57,73 @@ System.out.println("not a leap year");
 }else{
     System.out.println("invaild year");
 }
+
+Scanner ch = new Scanner(System.in);
+char v = ch.next().charAt(0);
+if(v=='a'||v=='e'||v=='i'||v=='o'||v=='u'){
+    System.out.println("vowel");
+}
+*/
+
+//quetion number 1
+/*System.out.println("enter any interger ");
+Scanner ch = new Scanner(System.in);
+int a = ch.nextInt();
+if(a%2==0){
+    System.out.println("its even");
+}else {
+    System.out.println("its odd");
+}*/
+
+//question no 2
+/*System.out.println("enter two interger ");
+Scanner ch = new Scanner(System.in);
+int  a = ch.nextInt();
+int  b = ch.nextInt();
+if (a>b){
+    System.out.println("a is greater");
+}else{
+    System.out.println("b is greater ");
+}*/
+// question no 3
+/*System.out.println("enter a char ");
+Scanner ch = new Scanner(System.in);
+char v = ch.next().charAt(0);
+
+switch (v) {
+    case  'a':
+    case 'e':
+    case  'i':
+    case  'o':
+    case 'u':
+System.out.println("vowel");
+        break;
+
+    default:
+        System.out.println("consonent");
+}*/
+//question no 4
+
+/*System.out.println("enter the value of n ");
+Scanner ch = new Scanner(System.in);
+int  n = ch.nextInt();
+for(int i=1;i<=n;i++){
+    System.out.print(i +" ");
+}*/
+//question no 5
+/*System.out.println("enter the value of n ");
+Scanner ch = new Scanner(System.in);
+int  n = ch.nextInt();
+int sum= 0;
+int i=1;
+
+while(i<=n){
+sum+=i;
+i++;
+}
+System.out.println("the sum is "+ sum);*/
+
+//question no 6
+
     }
 }
